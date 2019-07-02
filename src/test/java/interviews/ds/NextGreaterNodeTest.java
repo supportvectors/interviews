7de42dc5d5590585d8c7630466ce72dbc7e80621
Asif Qamar <asif@supportvectors.com>
2019-07-02 13:29:27 -0700

    Trivial fix

--- src/test/java/interviews/ds/NextGreaterNodeTest.java
+++ src/test/java/interviews/ds/NextGreaterNodeTest.java
@@ -18,15 +18,4 @@ class NextGreaterNodeTest {
         System.out.printf("Greater Node List: %n%s%n", greaterNodeList);
     }
 
-    @Test
-    void testMethod2() {
-        System.out.println(" NEXT GREATER TEST ");
-        final LinkedList list = new LinkedList();
-        Arrays.stream(new int[] { 1, 7, 5, 1, 9, 2, 5, 1 })
-              .forEach(value -> list.append(value));
-        System.out.printf("Original List: %n%s%n", list);
-        LinkedList greaterNodeList = NextGreaterNode.method2(list);
-
-        System.out.printf("Greater Node List: %n%s%n", greaterNodeList);
-    }
 }
